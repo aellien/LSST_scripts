456a208390a47581de748fecf356e519bbe86986
aellien <a.r.j.ellien@uva.nl>
2021-12-23 19:08:42 +0100

    fixes

--- launch_dawis.sh
+++ launch_dawis.sh
@@ -6,10 +6,10 @@
 # noise_00761_0000120_0.05_g_2Mpc.rebin.fits
 # noise_00761_0000078_0.05_g_2Mpc.rebin.fits
 #
-path_simulations="/home/ellien/LSST_ICL/simulations/out1/"
+path_simulations="/home/ellien/LSST_ICL/simulations/out1/*rebin*"
 
 
-for file in $( ls ../simulations/out1/*rebin* )
+for file in $( ls $path_simulations )
 do
       echo "Launch Dawis on file ${file}"
       qsub qsub_dawis.sh -v ncl=${a:12:7},nf=${file}
