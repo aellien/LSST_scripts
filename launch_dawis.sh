3e849ef727b5d688b585a087e29a37f69295d2b8
aellien <a.r.j.ellien@uva.nl>
2021-12-24 00:43:44 +0100

    fix path pb

--- launch_dawis.sh
+++ launch_dawis.sh
@@ -9,9 +9,9 @@
 path_simulations="/home/ellien/LSST_ICL/simulations/out1/*rebin*"
 
 
-for file in $( ls $path_simulations )
+for file in noise_00761_0000385_0.05_g_2Mpc.rebin.fits noise_00761_0000174_0.05_g_2Mpc.rebin.fits noise_00761_0000126_0.05_g_2Mpc.rebin.fits noise_00761_0000120_0.05_g_2Mpc.rebin.fits noise_00761_0000078_0.05_g_2Mpc.rebin.fits
 do
       echo "Launch Dawis on file ${file}"
-      qsub qsub_dawis.sh -v ncl=${file:51:7},nf=${file}
+      qsub qsub_dawis.sh -v ncl=${file:12:7},nf=${file}
       sleep 2
 done
