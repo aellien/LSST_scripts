721a15684cd01067adc0caa74c5b093932313f6a
aellien <a.r.j.ellien@uva.nl>
2021-12-23 19:01:35 +0100

    fixes

--- launch_dawis.sh
+++ launch_dawis.sh
@@ -12,6 +12,6 @@ path_simulations="/home/ellien/LSST_ICL/simulations/out1/"
 for file in $( ls ../simulations/out1/*rebin* )
 do
       echo "Launch Dawis on file ${file}"
-      qsub qsub_dawis.sh -v ncl=${a:12:7} nf=${file}
-      sleep 5
+      qsub qsub_dawis.sh -v ncl=${a:12:7},nf=${file}
+      sleep 2
 done
