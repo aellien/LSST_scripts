b9089944cfaf217ff25b23f03f557fc1fa459a79
aellien <a.r.j.ellien@uva.nl>
2022-01-21 15:33:27 +0100

    change par

--- dawis_LSST.py
+++ dawis_LSST.py
@@ -8,12 +8,12 @@ outdir = '/n03data/ellien/LSST_ICL/wavelets/out4'
 n_cpus = 6 # Number of CPUs
 tau = 0.8   # Relative Threshold
 gamma = 0.5   # Attenuation (CLEAN) factor
-ceps = 1E-5    # Convergence value for epsilon
+ceps = 1E-6    # Convergence value for epsilon
 n_levels = 11    # Number of wavelet scales
 min_span = 1    # Minimum of wavelet scales spanned by an interscale tree (must be >= 1)
 max_span = 3    # Maximum number of wavelet scales spanned by an interscale tree
 lvl_sep_big = 6     # Scale at wich mix_span & max_span are set to 1, and gamma to 1
-extent_sep = 0.1    # Ratio n_pix/vignet under which the Haar wavelet is used for restoration
+extent_sep = 1E-10    # Ratio n_pix/vignet under which the Haar wavelet is used for restoration
 lvl_sep_lin = -1     # Wavelet scale under which the Haar wavelet can be used for restoration
 max_iter = 1500      # Maximum number of iterations
 data_dump = True    # Write data at each iteration /!\ demands lot of space on hardware /!\
