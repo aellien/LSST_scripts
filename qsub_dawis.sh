76642acf67eab5e775d8cc1d29686ed775ada26b
aellien <a.r.j.ellien@uva.nl>
2022-01-21 16:07:57 +0100

    ch

--- qsub_dawis.sh
+++ qsub_dawis.sh
@@ -2,7 +2,7 @@
 #PBS -o /home/ellien/LSST_ICL/logs/icl_LSST_${ncl}.out
 #PBS -j oe
 #PBS -N icl_LSST
-#PBS -l nodes=n02:ppn=6,walltime=96:00:00
+#PBS -l nodes=1:ppn=6,walltime=96:00:00
 #PSB -S /bin/bash
 
 module load intelpython/3-2020.4
