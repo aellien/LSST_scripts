68b6dbb90f64220aaf1583c54fea14caedefb016
aellien <a.r.j.ellien@uva.nl>
2022-03-10 19:54:13 +0100

    Updated renormalize_images.py

--- renormalize_images.py
+++ renormalize_images.py
@@ -6,16 +6,13 @@ from scipy.ndimage import gaussian_filter
 if __name__ == '__main__':
 
     # Paths, lists & variables
-    path_data = '/home/ellien/LSST_ICL/simulations/out1'
+    path_data = '/home/ellien/LSST_ICL/simulations/out2/HorizonAGN'
     path_scripts = '/home/ellien/LSST_ICL/scripts'
-    path_plots = '/home/ellien/LSST_ICL/plots/out1'
-    path_wavelets = '/n03data/ellien/LSST_ICL/wavelets/out1/'
+    path_plots = '/home/ellien/LSST_ICL/plots/out2'
+    path_wavelets = '/n03data/ellien/LSST_ICL/wavelets/out2/'
 
-    nfl = [ 'noise_00761_0000078_0.05_g_2Mpc.rebin.fits', \
-            'noise_00761_0000120_0.05_g_2Mpc.rebin.fits', \
-            'noise_00761_0000126_0.05_g_2Mpc.rebin.fits', \
-            'noise_00761_0000174_0.05_g_2Mpc.rebin.fits', \
-            'noise_00761_0000385_0.05_g_2Mpc.rebin.fits'  ]
+    nfl = [ '00761_0000001_0.05_xy_r_4Mpc_mu30.rebin.fits', \
+            '00761_0000001_0.05_yz_r_4Mpc_mu30.rebin.fits' ]
 
     for nf in nfl:
 
@@ -32,9 +29,9 @@ if __name__ == '__main__':
         hduo = fits.PrimaryHDU( oim, header = oheader )
         hduo.writeto( os.path.join( path_data, onf ), overwrite = True )
 
-        fim = np.copy(im)
-        fim = gaussian_filter( fim, 1.0 )
+        #fim = np.copy(im)
+        #fim = gaussian_filter( fim, 1.0 )
 
-        fnf = nf[:-4] + 'gauss.fits'
-        hduo = fits.PrimaryHDU( fim, header = header )
-        hduo.writeto( os.path.join( path_data, fnf ), overwrite = True )
+        #fnf = nf[:-4] + 'gauss.fits'
+        #hduo = fits.PrimaryHDU( fim, header = header )
+        #hduo.writeto( os.path.join( path_data, fnf ), overwrite = True )
