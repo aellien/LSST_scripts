83269ad0e4fb16201968704b7b9786bd6d74a404
aellien <a.r.j.ellien@uva.nl>
2022-03-11 12:01:50 +0100

    fix path

--- dawis_LSST.py
+++ dawis_LSST.py
@@ -5,7 +5,7 @@ import shutil
 
 indir = '/n03data/ellien/LSST_ICL/simulations/out2/HorizonAGN'
 infile = sys.argv[1]
-outdir = '/n03data/ellien/LSST_ICL/wavelets/out2/HorizonAGN/run1'
+outdir = '/n03data/ellien/LSST_ICL/wavelets/out2/HorizonAGN/run2'
 n_cpus = 6 # Number of CPUs
 tau = 0.8   # Relative Threshold
 gamma = 0.5   # Attenuation (CLEAN) factor
