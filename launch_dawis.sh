27de3890ac64dbc0a62ba12a33f3cd256cdb61c3
aellien <a.r.j.ellien@uva.nl>
2022-03-11 12:05:18 +0100

    fix small detail on file name

--- launch_dawis.sh
+++ launch_dawis.sh
@@ -7,6 +7,6 @@
 for file in 00761_0000001_0.05_xy_r_4Mpc_mu30.rebin.norm.fits 00761_0000001_0.05_yz_r_4Mpc_mu30.rebin.norm.fits
 do
       echo "Launch Dawis on file ${file}"
-      qsub qsub_dawis.sh -v ncl=${file:12:7},nf=${file}
+      qsub qsub_dawis.sh -v ncl=${file:1:21},nf=${file}
       sleep 2
 done
