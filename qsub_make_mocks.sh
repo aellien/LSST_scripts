cf473ee6277062c904af36e74efe0158ad410db7
aellien <a.r.j.ellien@uva.nl>
2022-04-07 02:39:20 +0200

    qsub_make_mocks

---
+++ qsub_make_mocks.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#PBS -j oe
+#PBS -N maker
+#PBS -l nodes=1:ppn=1,walltime=24:00:00
+#PSB -S /bin/bash
+
+module load intelpython/3-2020.4
+python /home/ellien/LSST_ICL/LSST_scripts/make_mocks.py
+
+
+exit 0
