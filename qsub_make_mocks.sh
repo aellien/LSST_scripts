791fe8fe76215cbf8037613949f8c549fec48a54
aellien <a.r.j.ellien@uva.nl>
2022-04-07 02:40:26 +0200

    bugfix

--- qsub_make_mocks.sh
+++ qsub_make_mocks.sh
@@ -5,7 +5,7 @@
 #PSB -S /bin/bash
 
 module load intelpython/3-2020.4
-python /home/ellien/LSST_ICL/LSST_scripts/make_mocks.py
+python /home/ellien/LSST_ICL/LSST_scripts/make_mocks_fits.py
 
 
 exit 0
