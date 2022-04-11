150b3d14f97da1d0ef921b6eb2bf5669c5fc989f
aellien <a.r.j.ellien@uva.nl>
2022-04-11 13:01:20 +0200

    relaunch missing ones

--- launch_dawis.sh
+++ launch_dawis.sh
@@ -2,11 +2,12 @@
 #
 # HorizonAGN  Hydrangea  Magneticum  TNG-100
 
-path="/n03data/ellien/LSST_ICL/simulations/out2/"
+path="/n03data/ellien/LSST_ICL/simulations/out2"
 
 for dir in TNG-100
 do
-    for file in $path/$dir/*norm.fits
+    #for file in $path/$dir/*norm.fits
+    for file in $path/$dir/00099_0000005_0.05_xz_r_4Mpc_mu30.rebin.norm.fits $path/$dir/00099_0000004_0.05_yz_r_4Mpc_mu30.rebin.norm.fits $path/$dir/00099_0000004_0.05_xy_r_4Mpc_mu30.rebin.norm.fits 
     do
           echo "Launch Dawis on file ${file}"
           n=$(basename "$file")
