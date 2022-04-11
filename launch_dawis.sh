ddfd2a9b677811ce1d2e7e1ecd59b3889603e325
aellien <a.r.j.ellien@uva.nl>
2022-04-11 14:01:10 +0200

    relaunch hydrangea

--- launch_dawis.sh
+++ launch_dawis.sh
@@ -4,11 +4,11 @@
 
 path="/n03data/ellien/LSST_ICL/simulations/out2"
 
-for dir in TNG-100
+for dir in Hydrangea
 do
     #for file in $path/$dir/*norm.fits
-    for file in $path/$dir/00099_0000005_0.05_xz_r_4Mpc_mu30.rebin.norm.fits $path/$dir/00099_0000004_0.05_yz_r_4Mpc_mu30.rebin.norm.fits $path/$dir/00099_0000004_0.05_xy_r_4Mpc_mu30.rebin.norm.fits 
-    do
+    for file in $path/$dir/*norm.fits
+
           echo "Launch Dawis on file ${file}"
           n=$(basename "$file")
           qsub qsub_dawis.sh -v ncl=${n:0:21},nf=${n},dir=${dir}
