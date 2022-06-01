066e0d5688f38a64bdd921ee8f6477d9956b5b2e
aellien <a.r.j.ellien@uva.nl>
2022-06-01 11:08:30 +0200

    e5

--- atom_props.py
+++ atom_props.py
@@ -95,4 +95,4 @@ if __name__ == '__main__':
                     tol.append(o)
                     titl.append(itl[j])
 
-            df = average_size_atom( ol, n_levels )
+            df = average_size_atom( tol, n_levels )
