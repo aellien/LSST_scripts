794c4a6ff85e9fc6ee767a224ab220adffc7d44e
aellien <a.r.j.ellien@uva.nl>
2022-06-02 15:03:09 +0200

    e58

--- make_results.py
+++ make_results.py
@@ -373,9 +373,133 @@ def make_results_hardsep( oim, path_wavelets, lvl_sep_big, n_hard_icl, rc, ricl,
     return rdc, icl, gal, uicl, res, rim
 
 
-def make_results_sizesep(  ):
+def make_results_sizesep( oim, nfp, lvl_sep_big, n_hard_icl, rc, ricl, nf, xs, ys, n_levels ):
 
-    return rdc, icl, gal, uicl, res, rim
+    # path, list & variables
+    res = np.zeros( (xs, ys) )
+    icl = np.zeros( (xs, ys) )
+    uicl = np.zeros( (xs, ys) )
+    gal = np.zeros( (xs, ys) )
+    rim = np.zeros( (xs, ys) )
+    unclass = np.zeros( (xs, ys) )
+
+    lres = []
+    licl = []
+    luicl = []
+    lgal = []
+    lrim = []
+    lunclass = []
+
+    atom_gal = []
+    coo_gal = []
+
+    rdc_array = np.zeros( (xs, ys, n_levels) )
+
+    xc = xs / 2.
+    yc = ys / 2.
+
+    ol, itl = read_image_atoms( nfp, verbose = True )
+    df_sizes = average_size_atom( ol, n_levels )
+    print(df)
+
+    lvl_sx = np.argmax( derivative( df_sizes['<sx_n>'].to_numpy(), 2, n_levels )) + 2
+    lvl_sy = np.argmax( derivative( df_sizes['<sy_n>'].to_numpy(), 2, n_levels )) + 2
+
+    sx = df_sizes['<sx>'][lvl_sx]
+    sy = df_sizes['<sy>'][lvl_sy]
+
+    print(lvl_sx, sx)
+    print(lvl_sy, sy)
+
+    for j, object in enumerate(ol):
+
+        x_min, y_min, x_max, y_max = object.bbox
+        lvlo = object.level
+        xco = itl[j].interscale_maximum.x_max
+        yco = itl[j].interscale_maximum.y_max
+
+        flag_icl = False
+        flag_gal = False
+        flag_uicl = False
+
+        # ICL or gal based on size
+        if x_max - x_min >= sx or y_max - y_min >= sy:
+
+            if object.level >= lvl_sep_big:
+                icl[ x_min : x_max, y_min : y_max ] += object.image
+            else:
+                icl[ x_min : x_max, y_min : y_max ] += object.image * gamma
+
+        else:
+
+            if object.level >= lvl_sep_big:
+                gal[ x_min : x_max, y_min : y_max ] += object.image
+            else:
+                gal[ x_min : x_max, y_min : y_max ] += object.image * gamma
+            atom_gal.append(object)
+            coo_gal.append([xco, yco])
+
+        # All object dc
+        if object.level >= lvl_sep_big:
+            rdc_array[ x_min : x_max, y_min : y_max, object.level ] += object.image
+        else:
+            rdc_array[ x_min : x_max, y_min : y_max, object.level ] += object.image * gamma
+
+    '''
+    bcg = np.copy(gal)
+
+    for i, ogal in enumerate(atom_gal):
+
+        x_min, y_min, x_max, y_max = ogal.bbox
+        xco, yco = coo_gal[i]
+        lvlo = ogal.level
+
+        if lvlo <= 4:
+
+            if np.sqrt( ( xco - xc )**2 + ( yco - yc )**2 ) > rc * 2**(lvlo-1):
+
+                if x_max - x_min <= 300 or y_max - y_min <= 300:
+
+                    bcg[ x_min : x_max, y_min : y_max ] -= ogal.image * gamma
+
+                elif np.sqrt( ( xco - xc )**2 + ( yco - yc )**2 ) > ricl:
+
+                    bcg[ x_min : x_max, y_min : y_max ] -= ogal.image * gamma
+
+    satgal = np.copy(gal)
+    satgal -= bcg
+    iclbcg = np.copy(icl)
+    iclbcg += bcg
+    '''
+
+    # Datacube
+    rdc = d.datacube( rdc_array, dctype = 'NONE', fheader = header )
+    rim = np.sum( rdc_array, axis = 2 )
+    res = oim - rim
+
+    # write to fits
+    hduo = fits.PrimaryHDU(res)
+    hduo.writeto( nfp + 'results.residuals.fits', overwrite = True )
+
+    hduo = fits.PrimaryHDU(icl)
+    hduo.writeto( nfp + 'results.icl.sizesep.fits', overwrite = True )
+
+    hduo = fits.PrimaryHDU(gal)
+    hduo.writeto( nfp + 'results.gal.sizesep.fits', overwrite = True )
+
+    hduo = fits.PrimaryHDU(rim)
+    hduo.writeto( nfp + 'results.rim.fits', overwrite = True )
+
+    #hduo = fits.PrimaryHDU(satgal)
+    #hduo.writeto(os.path.join( path_wavelets, ''.join( ( nf, 'results.gal.hardsepBCG.fits') )), overwrite = True )
+
+    #hduo = fits.PrimaryHDU(iclbcg)
+    #hduo.writeto(os.path.join( path_wavelets, ''.join( ( nf, 'results.iclbcg.hardsepBCG.fits') )), overwrite = True )
+
+    ##rdc.to_fits( os.path.join( path_wavelets, ''.join( ( nf, 'results.rdc.fits') )), overwrite = True )
+
+
+    return rdc, icl, gal, res, rim
 
 if __name__ == '__main__':
 
@@ -415,19 +539,9 @@ if __name__ == '__main__':
             nf = split[-1]
             nfp = os.path.join( path_wavelets, dir, 'run1', nf[:-4] )
 
-            ol, itl = read_image_atoms( nfp, verbose = True )
-            df_sizes = average_size_atom( ol, n_levels )
+            rdc, icl, gal, res, rim = make_results_sizesep( oim, nfp, lvl_sep_big, n_hard_icl, rc, ricl, nf, xs, ys, n_levels )
 
-            print(df_sizes)
 
-            lvl_x, min_x = find_minimum_size_z( df_sizes['<sx_n>'].to_numpy(), 2, n_levels )
-            lvl_y, min_y = find_minimum_size_z( df_sizes['<sy_n>'].to_numpy(), 2, n_levels )
-
-            print( lvl_x, min_x )
-            print( lvl_y, min_y )
-
-            print( np.argmax( derivative( df_sizes['<sx_n>'].to_numpy(), 2, n_levels )) + 2 )
-            print( np.argmax( derivative( df_sizes['<sy_n>'].to_numpy(), 2, n_levels )) + 2 )
 
             n_coregal = 3
             #cat = make_galaxy_catalog( oim, nf, n_levels, n_sig_gal = 50, level_gal = 3 )
