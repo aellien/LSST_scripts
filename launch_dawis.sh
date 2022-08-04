d037ab48ca0dd8ac1fe70c994cd2bc9568073a89
ellien <ellien@iap.fr>
2022-08-04 18:21:41 +0200

    bug fix

--- launch_dawis.sh
+++ launch_dawis.sh
@@ -2,16 +2,16 @@
 #
 # HorizonAGN  Hydrangea  Magneticum  TNG-100
 
-path="/n03data/ellien/LSST_ICL/simulations" #out3
+path="/n03data/ellien/LSST_ICL/simulations/out3"
 
-for dir in out3
-do
+#for dir in TNG-100
+#do
     #for file in $path/$dir/*norm.fits
-    for file in $path/$dir/*norm.fits
+    for file in $path/*norm.fits
     do
           echo "Launch Dawis on file ${file}"
           n=$(basename "$file")
           qsub qsub_dawis.sh -v ncl=${n:0:21},nf=${n},dir=${dir}
           sleep 2
     done
-done
+#done
