7892a0298fdf6cd07fc5a04fdd08f428264d5bb3
ellien <ellien@iap.fr>
2023-03-06 13:56:38 +0100

    sep ICL 250 kpc sep BCG 250 kpc

--- make_results.py
+++ make_results.py
@@ -1000,7 +1000,7 @@ if __name__ == '__main__':
     lvl_sep_big = 6
     lvl_sep_l = [ 6 ]
     size_sep_icl_l = [ 250 ] # separation radius gal/icl kpc
-    size_sep_bcg_l = [ 250 ] # separation radius bcg/icl kpc
+    size_sep_bcg_l = [ 60 ] # separation radius bcg/icl kpc
     sbt_l = [ 26. ]# [  26, 26.5, 27, 27.5, 28. ]
     err_size = 0.2
     pixscale = 0.8 # ''/pixel
